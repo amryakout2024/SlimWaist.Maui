fe8187828c8119aeef7b3fbe10dd74d6d624f046
amr yakout <amrnewstory@gmail.com>
2025-02-09 23:21:07 +0300

    same as old diet app

--- SlimWaist/App.xaml.cs
+++ SlimWaist/App.xaml.cs
@@ -1,15 +1,52 @@
-﻿namespace SlimWaist
+﻿using SlimWaist.Models;
+using SlimWaist.ViewModels;
+using SlimWaist.Views;
+
+namespace SlimWaist
 {
     public partial class App : Application
     {
-        public App()
+        private readonly AppShellVM _appShellVM;
+
+        private readonly DataContext _dataContext;
+        //private readonly TabbedFoodsVM _tabbedFoodsVM;
+
+        public App(AppShellVM appShellVM, DataContext dataContext)
         {
             InitializeComponent();
+
+            _appShellVM = appShellVM;
+
+            _dataContext = dataContext;
+
+            MainPage = new AppShell(_appShellVM);
+
+            //MainPage = new TabbedPage();
+
+            InitializeDatabase();
+
+            //Helpers.ReadExcel.ReadExcelSheet();
+
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
+        private async Task InitializeDatabase()
         {
-            return new Window(new AppShell());
+            try
+            {
+                var BodyActivityTest = _dataContext.Database.Table<BodyActivity>().FirstOrDefault();
+
+                if (BodyActivityTest == null)
+                {
+                    await _dataContext.init();
+                }
+            }
+            catch (Exception e)
+            {
+                await _dataContext.init();
+            }
+
         }
+
+
     }
 }
--- SlimWaist/AppShell.xaml.cs
+++ SlimWaist/AppShell.xaml.cs
@@ -1,10 +1,50 @@
-﻿namespace SlimWaist
+﻿using SlimWaist.ViewModels;
+using SlimWaist.Views;
+
+namespace SlimWaist
 {
     public partial class AppShell : Shell
     {
-        public AppShell()
+        private readonly AppShellVM _appShellVM;
+
+        public AppShell(AppShellVM appShellVM)
         {
             InitializeComponent();
+
+            _appShellVM = appShellVM;
+
+            BindingContext = _appShellVM;
+
+            RegisterRoutes();
+
+            Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
+
         }
+        private readonly static Type[] _routablePageTypes =
+        [
+            typeof(RegisterPage),
+            typeof(SignUpPage),
+            typeof(HomePage),
+            typeof(RegimesListPage),
+            typeof(FoodsPage),
+            typeof(ItemPage),
+            typeof(CartPage),
+            typeof(MealDetailPage),
+            typeof(DayMealsPage),
+            typeof(AddFoodPage),
+        ];
+        //private readonly static Type[] _routablePageTypes =
+        //[
+        //    typeof(RegisterPage),
+        //];
+
+        private static void RegisterRoutes()
+        {
+            foreach (var pageType in _routablePageTypes)
+            {
+                Routing.RegisterRoute(pageType.Name, pageType);
+            }
+        }
+
     }
 }
