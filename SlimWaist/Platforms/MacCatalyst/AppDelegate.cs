c946288d9890be2813833bdf6d1c7a76ff5ca580
amr yakout <amrnewstory@gmail.com>
2025-02-09 11:42:28 +0300

    Add project files.

---
+++ SlimWaist/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SlimWaist
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
