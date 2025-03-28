1332d6bb906e84dfba1737131a16ec2ea14cee62
amr yakout <amrnewstory@gmail.com>
2025-03-28 10:33:30 +0300

    styling setting page finish V3

--- SlimWaist/Platforms/Android/MainActivity.cs
+++ SlimWaist/Platforms/Android/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Content.Res;
 using Android.OS;
 
 namespace SlimWaist
@@ -7,5 +8,32 @@ namespace SlimWaist
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Disable font scaling
+            Resources.Configuration.FontScale = 1.0f;
+            // Your existing code goes here
+        }
+
+        //disable font change in user device
+        public override Resources? Resources
+        {
+            get
+            {
+                Resources resource = base.Resources;
+                Configuration configuration = new Configuration();
+                configuration.SetToDefaults();
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.NMr1)
+                {
+                    return CreateConfigurationContext(configuration).Resources;
+                }
+                else
+                {
+                    resource.UpdateConfiguration(configuration, resource.DisplayMetrics);
+                    return resource;
+                }
+            }
+        }
     }
 }
